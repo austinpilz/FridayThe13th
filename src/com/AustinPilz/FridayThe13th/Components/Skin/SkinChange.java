ba94827e774be48879f558472d7a9d7fbe207874
BuildTools <unconfigured@null.spigotmc.org>
2017-08-05 11:48:47 -0700

    Added maven support.

--- src/com/AustinPilz/FridayThe13th/Components/Skin/SkinChange.java
+++
@@ -1,9 +0,0 @@
-package com.AustinPilz.FridayThe13th.Components.Skin;
-
-import com.AustinPilz.FridayThe13th.Components.Enum.F13Skin;
-
-public interface SkinChange {
-    public void apply(F13Skin skin);
-
-    public void revert();
-}
